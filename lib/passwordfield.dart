a00bff3c14098b367cd06523b97a6d4283ecae5b
tanmoy27112000 <tkarmakar27112000@gmail.com>
2020-10-19 01:10:00 +0530

    wip

--- lib/passwordfield.dart
+++ lib/passwordfield.dart
@@ -174,7 +174,10 @@ class PasswordFieldState extends State<PasswordField> {
               obscureText: obscureText,
               autofocus: widget.autoFocus,
               decoration: InputDecoration(
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 16.0,
+                    vertical: 8.0,
+                  ),
                   border: widget.backgroundColor != null
                       ? InputBorder.none
                       : widget.border,
