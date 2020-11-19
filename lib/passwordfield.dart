a74f32170cf7a58f824af57ef95284cae6dcb8c2
tanmoy27112000 <tkarmakar27112000@gmail.com>
2020-11-19 13:24:39 +0530

    wip

--- lib/passwordfield.dart
+++ lib/passwordfield.dart
@@ -226,11 +226,11 @@ class PasswordFieldState extends State<PasswordField> {
                                       widget.isObscured
                                           ? Icon(Icons.remove_red_eye)
                                           : Icon(Icons.remove_red_eye_outlined),
-                                      IconButton(
-                                          icon: Icon(Icons.close),
-                                          onPressed: () {
+                                      InkWell(
+                                          onTap: () {
                                             widget.controller.clear();
-                                          })
+                                          },
+                                          child: Icon(Icons.close)),
                                     ],
                                   ),
                                 )
